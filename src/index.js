8b00b37ee6b9249fa825c490328e94358c81bc93
lukejagg <lukejagg@gmail.com>
2024-10-17 18:02:33 -0700

    Added paige

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import executeInDevMode from '@speckai/paige';
+executeInDevMode('http://localhost:8080/npm/script.js');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
